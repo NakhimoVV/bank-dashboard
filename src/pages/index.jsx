169bc3cd0bc6589685febfa7870aed3d1dbb112b
Aleksey Nakhimov <tj1993@bk.ru>
2025-08-26 22:36:46 +0300

    add resources

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import '@/styles'
 import { Head } from "minista"
 
 export default function () {
@@ -7,6 +8,7 @@ export default function () {
         <title>Hello!</title>
       </Head>
       <h1>Hello!</h1>
+      <p>Hi man!</p>
     </>
   )
 }
