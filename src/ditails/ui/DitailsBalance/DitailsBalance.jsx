fc00660b106dc5e148987a1c5853f0aeed6266da
NakhimoVV <tj1993@bk.ru>
2025-08-29 17:31:26 +0300

    minimal different in DitailsBalance.jsx

--- src/ditails/ui/DitailsBalance/DitailsBalance.jsx
+++ src/ditails/ui/DitailsBalance/DitailsBalance.jsx
@@ -1,4 +1,5 @@
 import './DitailsBalance.scss'
+import Button from "@/components/Button";
 
 const DitailsBalance = (props) => {
   const {} = props
@@ -6,10 +7,16 @@ const DitailsBalance = (props) => {
   return (
     <div className="ditails-balance">
       <div className="ditails-balance__left">
-        left
+        <p>Your balance:</p>
+        <strong>25165</strong>
+        <p>3 cards</p>
       </div>
       <div className="ditails-balance__right">
-        right
+        <Button
+          label = "Details"
+          iconName = "arrow-right"
+          iconPosition = "after"
+        />
       </div>
     </div>
   )
